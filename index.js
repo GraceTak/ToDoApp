ee2b58935b502b0c523133b1e514b6152245b787
Charis Taktikou <haristak@gmail.com>
2024-06-27 09:57:07 +0300

    get requests

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.status(200).json({ msg: "hello there" });
-});
+const router = require("./routes");
+app.use("/api", router);
 
 const port = 5000;
 app.listen(port, () => {
